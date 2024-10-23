adbe0d810aa87a599d5cd66ad2ce9074de918709
Balirwa Alvin Daniel <159263723+naughtyug@users.noreply.github.com>
2024-10-23 15:23:38 +0300

    BADD-APP (REACT)
    This is a react application under the next.js frame work

---
+++ app/page.js
@@ -0,0 +1,22 @@
+function HomePage() {
+    return (
+      <div>
+        <nav>
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+        </nav>
+        <div>
+          <h1>Welcome to My Next.js App</h1>
+          <p>This is an example of embedding HTML within your component.</p>
+          <ul>
+            <li>Feature One</li>
+            <li>Feature Two</li>
+            <li>Feature Three</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+  
+  export default HomePage;
+  
